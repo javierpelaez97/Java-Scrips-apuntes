0dfd14eb2a1fa2e9d848ee8846f785ca29ec7504
javierpelaez97 <javierpelaezgimenez@gmail.com>
2023-11-12 21:30:16 +0100

    Actualización 12/11/23

--- asyncronia/promesa.js
+++ asyncronia/promesa.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const request = fetch(`https://restcountries.com/v3.1/region/europe`)
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+const request = fetch(`https://restcountries.com/v3.1/region/europe`)  //  Creamos una variable y la llamamos con fetch a la API
+.then(response => response.json())          // le indicamos la promesa de que va a pasar los archivos JSOn
+.then(data => console.log(data))            // Le indicamos la promesa de que va a hacer algo cuando acabe
+.catch(error => console.error(error));      // Si no funciona nuestra promesa que lance un mensaje de error.
